f06a0f616b025bd9f3d5d39690ccd9d880e3c67b
laplandin <deniskakhv@gmail.com>
2016-08-27 16:16:15 +0300

    script for popup-s action

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 $(document).ready( function() {
-  $('.portfolio-action').click( function() {
-                                $('.overlay').css('display', 'block');
-                                $('.portfolio-popup').css('display','block');
-                              });
+  
+  $('.portfolio-popup').addClass( function(index) {
+    index++;
+    return 'popup' + index;
+  });
+
+  $('.portfolio-item').click( function() {
+    var index = 'popup' + ($(this).index() + 1);
+    $('.overlay').css('display', 'block');
+    $('.portfolio-popup' + '.' + index).css('display','block');
+
+    $(window).keydown( function(key) {
+      if (key.which == 27) {
+        $('.' + index).css('display','none');
+        $('.overlay').css('display', 'none');
+        console.log(key.which);
+      }
+    });
+  });
 
   $('.overlay').click( function() {
-                      $('.overlay').css('display', 'none');
-                      $('.portfolio-popup').css('display','none');
-                    });
+    $('.overlay').css('display', 'none');
+    $('.portfolio-popup').css('display','none');
+  });
 })
